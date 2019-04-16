618af438571cfc06018e9a0552a5c992a2a96259
RAED <rrrr53@hotmail.com>
2019-04-17 01:12:45 +0300

    Update pages_gen.c

--- webif/pages_gen.c
+++ webif/pages_gen.c
@@ -408,6 +408,7 @@ int main(void)
 	fprintf(output_file, "\n");
 	fprintf(output_file, "#include \"pages.h\"\n");
 	fprintf(output_file, "\n");
+	fprintf(output_file, "#endif\n");
 
 #ifdef USE_COMPRESSION
 	// Calculate positions at which the values would be storred
