01e9046c1d4090cd2fc592490a7c03471782cce2
RAED <rrrr53@hotmail.com>
2019-04-17 00:20:34 +0300

    Update module-emulator-biss.h

--- module-emulator-biss.h
+++ module-emulator-biss.h
@@ -3,7 +3,6 @@
 
 #ifdef WITH_EMU
 
-#if defined(DVBCISSA_BISS2)
 #include <openssl/rsa.h>
 
 #define BISS2_MAX_RSA_KEYS 16
@@ -19,7 +18,6 @@ int8_t biss_emm(struct s_reader *rdr, const uint8_t *emm, uint32_t *keysAdded);
 uint16_t biss_read_pem(struct s_reader *rdr, uint8_t max_keys);
 #else
 int8_t biss_ecm(struct s_reader *rdr, uint16_t caid, const uint8_t *ecm, uint8_t *dw, uint16_t srvid, uint16_t ecmpid);
-#endif // DVBCISSA_BISS2
 
 #endif // WITH_EMU
 
